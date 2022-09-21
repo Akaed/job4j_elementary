c85d248b061db7eae4ab5f906c97208e1461d23f
Ilya <ilyayurch@gmail.com>
2022-09-21 05:48:48 +0400

    6.9. String - это массив [#504884 #345157]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -5,58 +5,49 @@ public class JavaNameValidator {
     public static boolean isNameValid(String name) {
         char currentCharacter;
         boolean valide = false;
-            for (int i = 0; i < name.length(); i++) {
-                currentCharacter = name.charAt(i);
-                if (Character.isDigit(currentCharacter)) {
-                    valide = true;
-                }
-                if (!(Character.isUpperCase(currentCharacter)) && i == 0 && isLowerLatinLetter(name)) {
-                    valide = true;
-                }
-                if (isSpecialSymbol(name)) {
-                    valide = true;
-                }
-                if (isUpperLatinLetter(name)) {
-                    valide = true;
+        if (!(name.isEmpty())) {
+            if (isLowerLatinLetter(name.charAt(0))) {
+                for (int i = 1; i < name.length(); i++) {
+                    currentCharacter = name.charAt(i);
+                    if (Character.isDigit(currentCharacter)) {
+                        valide = true;
+                    }
+                    if (isUpperLatinLetter(name.charAt(i))) {
+                        valide = true;
+                    }
+                    if (isLowerLatinLetter(name.charAt(i))) {
+                        valide = true;
+                    }
+                    if (isSpecialSymbol(name.charAt(i))) {
+                        valide = true;
+                    }
                 }
             }
-
-        if (name.isEmpty()) {
-            valide = false;
         }
         return valide;
     }
 
-    public static boolean isSpecialSymbol(String code) {
+    public static boolean isSpecialSymbol(int code) {
         boolean valide = false;
-        code = "_$";
-        Character code1 = code.charAt(2);
-        Character code2 = code.charAt(1);
-        if (code.equals(code1) || code.equals(code2)) {
+        if (code == 36 || code == 95) {
             valide = true;
         }
         return valide;
     }
 
-    public static boolean isUpperLatinLetter(String code) {
+    public static boolean isUpperLatinLetter(int code) {
         boolean valide = false;
-        for (int i = 0; i < code.length(); i++) {
-            int asci = code.codePointAt(i);
-            if (asci >= 65 || asci <= 99) {
+            if (code >= 65 && code <= 99) {
                 valide = true;
             }
-        }
         return valide;
     }
 
-    public static boolean isLowerLatinLetter(String  code) {
+    public static boolean isLowerLatinLetter(int  code) {
         boolean valide = false;
-        for (int i = 0; i < code.length(); i++) {
-            int asci = code.codePointAt(i);
-            if (asci >= 97 || asci <= 122) {
+            if (code >= 97 && code <= 122) {
                 valide = true;
             }
-        }
         return valide;
     }
 
